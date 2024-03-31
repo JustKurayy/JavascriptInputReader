4f5be51c3e878475dfa8d95f2117b1a3a170b44f
SirQuandale <302685723@student.rocmondriaan.nl>
2024-03-31 15:38:24 +0200

    added OS detection

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let osname = window.navigator.platform;
+document.querySelector('.console').innerHTML += osname + "<br>";
+
 document.addEventListener('keydown', function(event) {
     document.querySelector('.console').innerHTML += event.key + " ";
 });
